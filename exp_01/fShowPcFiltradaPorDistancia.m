5963747c443c64dcb4c901f2ca9de5bc6bf738b5
noteDell <m.wendh@gmail.com>
2022-12-29 12:36:18 -0300

    A interface GUI usada para segmentação finalizada, usando duas funções, uma baseada no threshhold de distãncia LiDARponto XYZ e outra usando a nativa do Matlab pcsegdist(). Funcionando ok!

---
+++ exp_01/fShowPcFiltradaPorDistancia.m
@@ -0,0 +1,24 @@
+function fShowPcFiltradaPorDistancia(pcOriginal, pcThreshold, handles)
+    
+% Cria um novo mapa de cores para os clusters
+figure;
+% Exibe a nuvem de pontos segmentada inteira:
+subplot(1,2,1);
+pcshow(pcOriginal.Location);
+numPontos= length(pcOriginal.Location); 
+msg= sprintf('PC original com %d pontos', numPontos);
+title(msg);
+xlabel('X (m)');
+ylabel('Y (m)');
+zlabel('Z (m)');
+
+subplot(1,2,2);
+pcshow(pcThreshold.Location);
+numPontos= length(pcThreshold.Location); 
+msg= sprintf('PC com %d pontos foi filtrada usando threshold: Min= %0.2fm e Max= %0.2fm', numPontos, handles.valThresholdMinDistance, handles.valThresholdMaxDistance);
+title(msg);
+xlabel('X (m)');
+ylabel('Y (m)');
+zlabel('Z (m)');
+
+end
