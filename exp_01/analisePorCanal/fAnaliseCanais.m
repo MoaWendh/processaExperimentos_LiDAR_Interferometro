4bb693b1a249deff9abddbd152052a6ebd5e41d2
noteDell <m.wendh@gmail.com>
2023-01-20 01:50:58 -0300

    Melhorias na função de segmentação e início da implementação da função fAnálise.m e fAnalisaCanal.m. Já estão sendo lidas as PCs segmentadas para análise.

--- exp_01/analisePorCanal/fAnaliseCanais.m
+++
@@ -1,6 +0,0 @@
-function fAnaliseCanais(handles)
-    close all;
-     
-    f = msgbox('Função ainda não elaborada!!!', 'Error','error');
-    
-end
--- exp_01/analisePorCanal/fShowPcFiltradaPorDistancia.m
+++ exp_01/analisePorCanal/fShowPcFiltradaPorDistancia.m
@@ -1,4 +1,4 @@
-function fShowPcFiltradaPorDistancia(pcOriginal, pcThreshold, handles, ctCanal, pcCompleta)
+function fShowPcFiltradaPorDistancia(pcCompleta, pcCanalSeparado, pcSegmentada, pcSegmentada01, handles, ctCanal)
     
 % Cria um novo mapa de cores para os clusters
 fig= figure;
@@ -15,23 +15,47 @@ zlabel('Z (m)');
 
 % Exibe a nuvem de pontos original como o canal separado:
 subplot(2,2,3);
-pcshow(pcOriginal.Location);
-numPontos= length(pcOriginal.Location); 
+pcshow(pcCanalSeparado.Location);
+numPontos= length(pcCanalSeparado.Location); 
 msg= sprintf('PC original - Canal %d com %d pontos', handles.cnSegmenta(ctCanal), numPontos);
 title(msg);
 xlabel('X (m)');
 ylabel('Y (m)');
 zlabel('Z (m)');
 
-subplot(2,2,[2 4]);
-pcshow(pcThreshold.Location);
-numPontos= length(pcThreshold.Location); 
-msg= sprintf('PC Segmentada - Canal %d com %d pontos - Threshold: Min= %0.2fm e Max= %0.2fm', handles.cnSegmenta(ctCanal), numPontos, handles.valThresholdMinDistance, handles.valThresholdMaxDistance);
-title(msg);
-xlabel('X (m)');
-ylabel('Y (m)');
-zlabel('Z (m)');
-fig.Position= [10, 40, 1600, 950];
+if (handles.habSegmentacaoNatMatlab)
+    % Exibe a PC segemtada na segunda etapa:
+    subplot(2,2,2);
+    pcshow(pcSegmentada01.Location);
+    numPontos= length(pcSegmentada01.Location); 
+    msg= sprintf('PC Segmentada - Canal %d com %d pontos - Threshold: Min= %0.2fm e Max= %0.2fm', handles.cnSegmenta(ctCanal), numPontos, handles.valThresholdMinDistance, handles.valThresholdMaxDistance);
+    title(msg);
+    xlabel('X (m)');
+    ylabel('Y (m)');
+    zlabel('Z (m)');
+    fig.Position= [10, 40, 1600, 950];
+    
+    % Exibe a PC segemtada na segunda etapa:
+    subplot(2,2,4);
+    pcshow(pcSegmentada.Location);
+    numPontos= length(pcSegmentada.Location); 
+    msg= sprintf('PC Segmentada - Canal %d com %d pontos - Threshold: Min= %0.2fm e Max= %0.2fm', handles.cnSegmenta(ctCanal), numPontos, handles.valThresholdMinDistance, handles.valThresholdMaxDistance);
+    title(msg);
+    xlabel('X (m)');
+    ylabel('Y (m)');
+    zlabel('Z (m)');
+    fig.Position= [10, 40, 1600, 950];
+else
+    subplot(2,2,[2 4]);
+    pcshow(pcSegmentada.Location);
+    numPontos= length(pcSegmentada.Location); 
+    msg= sprintf('PC Segmentada - Canal %d com %d pontos - Threshold: Min= %0.2fm e Max= %0.2fm', handles.cnSegmenta(ctCanal), numPontos, handles.valThresholdMinDistance, handles.valThresholdMaxDistance);
+    title(msg);
+    xlabel('X (m)');
+    ylabel('Y (m)');
+    zlabel('Z (m)');
+    fig.Position= [10, 40, 1600, 950];
+end
 
 fig.WindowState='maximized'
 end
